bdbc8e37a6628b9d90ad5ddff1fa6b72f12452b1
Kaio Alves <kaio.alves19@gmail.com>
2023-09-19 16:08:06 -0300

    Inicio do programa calculadora

---
+++ calculadora.py
@@ -0,0 +1,25 @@
+"""
+Implemente uma calculadora em Python, com instruções por linha de comando (CLI).
+ A calculadora deve possuir as seguintes funcionalidades:
+
+Deve ficar em um loop infinito até o momento em que o usuário inserir um texto vazio (apertar ENTER);
+Deve iniciar a calculadora no valor zero;
+Dentro do loop, executar as seguintes etapas:
+Ler um número;
+Ler uma operação (soma, subtração, multiplicação ou divisão);
+Ler outro número;
+Exibir o resultado;
+Pedir uma outra operação para continuar a conta ou “X” para limpar a calculadora;
+Se o usuário limpar a calculadora, ela deve recomeçar do zero.
+
+"""
+
+def calcula():
+
+    num1 = input("Preencha com um número")
+    operador = input("Preencha com um operador matématico")
+    num2 = input("Preencha com um segundo número")
+
+    while True:
+        pass
+
