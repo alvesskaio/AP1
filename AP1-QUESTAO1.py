79b30cd7cc601ddc5218b9ffd11bd5b16026b192
Gabriel Rosa <gabrielrosabarrto10@gmail.com>
2023-09-20 01:11:33 -0300

    Simplifiquei um pouco mais o codigo

--- AP1-QUESTAO1.py
+++ AP1-QUESTAO1.py
@@ -1,23 +1,23 @@
 import random
 import sys
 
-while True: # usuario ira defini o tamanho do dado, sendo 0 = tentar novamente, enter = sair
-    tamanho_dado = input('Forneça o tamanho do dado que será rolado (ENTER para sair): ')
+while True: # O usuario ira definir o tamanho do dado, sendo 0 = tentar novamente, enter = sair
+    tamanho_dos_dados = input('Forneça o tamanho do dado que será rolado (ENTER para sair): ')
 
-    if tamanho_dado == "":
+    if tamanho_dos_dados == "":
         print('Programa encerrado..')
         sys.exit()
 
     try:
-        tamanho_dado = float(tamanho_dado)
-        if tamanho_dado <= 0:
+        tamanho_dos_dados = float(tamanho_dos_dados)
+        if tamanho_dos_dados <= 0:
             print('O número deve ser maior que ZERO')
         else:
-            break  # para sair do loop caso essa condicao seja atendida
+            break  # Para sair do loop caso essa condicao seja atendida
     except ValueError:
         print("Por favor, digite um número válido.")
 
-def quantidade_de_dados():
+def quantidade_de_dados(): # Aqui iremos coletar a quantidade de dados descrita pelo usuario.
     while True:
         quantidade = input('Digite o número de dados (em branco == 1): ')
 
@@ -33,20 +33,19 @@ def quantidade_de_dados():
             except ValueError:
                 print("Por favor, digite um número válido.")
 
-def gerar_valores(tamanho_dado, quantidade):
-    return [random.randint(1, tamanho_dado) for _ in range(quantidade)]    
+def valores_random(tamanho_dos_dados, quantidade): # Gerara os valores aleatorios de acordo com os tamanhos de dados e a quantidade de dados que foi preenchida pelo usuario
+    return [random.randint(1, tamanho_dos_dados) for _ in range(quantidade)]    # Utilizei o underscore devido aos ensinamentos na aula, que posso ditar qualquer nome e so ocupa espaço na memoria.
 
 def imprimir_valores(valores):
     for i, valor in enumerate(valores, 1):
         print(f'Lançamento n{i} - {valor}')
 
-
 quantidade_dados = quantidade_de_dados() # O usuario ira definir quantos dados ele quer rodar
 
-
-valores_aleatorios = gerar_valores(tamanho_dado, quantidade_dados) # Gerar os valores aleatorios de acodro com a quantidade de dados desejados
+valores_aleatorios = valores_random(tamanho_dos_dados, quantidade_dados) # Gerar os valores aleatorios de acordo com a quantidade de dados desejados
 
 
 imprimir_valores(valores_aleatorios) # Imprimira os valores
 
-print(f'\n{quantidade_dados} dado(s) de {tamanho_dado} lados')
+print(f'\n{quantidade_dados} dado(s) de {tamanho_dos_dados} lados')
+print(valores_aleatorios, sep=' ')
